6edc2cf0a736aeb221e610222ec77b6518618866
Felix Eichenberger <felix.eichenberger@gyrotools.com>
2020-01-28 21:19:40 +0100

    Use mopidy url from config in the player iframe

--- src/app/player/components/player/player.component.ts
+++ src/app/player/components/player/player.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
+import { ElectronService } from 'src/app/electron/services';
+import { Observable } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-player',
   templateUrl: './player.component.html',
-  styleUrls: ['./player.component.scss']
+  styleUrls: ['./player.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PlayerComponent implements OnInit {
+  url$: Observable<SafeUrl>;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(electronService: ElectronService, private sanitizer: DomSanitizer) {
+    this.url$ = electronService.config.pipe(
+      filter(config => !!config),
+      map(config =>
+        this.sanitizer.bypassSecurityTrustResourceUrl(`http://${config.mopidy.host}:${config.mopidy.port}/iris/`)
+      )
+    );
   }
 
+  ngOnInit() {}
 }
