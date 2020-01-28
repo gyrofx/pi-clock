b270c2f0bdb316348d1ecee2629eef0b177e7f3d
Felix Eichenberger <felix.eichenberger@gyrotools.com>
2020-01-28 21:38:27 +0100

    Set electron windows to 800x480 (raspi display) and enable fullscreen

--- main.js
+++ main.js
@@ -42,7 +42,7 @@ function createWindow() {
   // Create the browser window.
   mainWindow = new BrowserWindow({
     width: 800,
-    height: 600,
+    height: 480,
     fullscreen: false,
     frame: false,
     webPreferences: {
